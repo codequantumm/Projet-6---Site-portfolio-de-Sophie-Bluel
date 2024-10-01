6d35c5a3772145954e3a29e3a8d00f4b89bbb18d
Nora Hamida <hamida.nora@gmail.com>
2024-10-01 11:09:45 +0200

    Ajout de la fonction afficherWorksDansModale, ajout bouton dans la modale et ajustement style modale

--- FrontEnd/assets/js/controllers/modal.js
+++ FrontEnd/assets/js/controllers/modal.js
@@ -2,6 +2,7 @@ let modal = null;
 
 const ouvrirModal = function (e) {
     e.preventDefault();
+    afficherWorksDansModale(); 
     console.log("lien cliqué pour ouvrir modale"); 
     
     const target = document.querySelector(e.target.getAttribute("href"));
@@ -39,3 +40,22 @@ const fermerModal = function (e) {
 document.querySelectorAll(".js-modal").forEach(a => {
     a.addEventListener("click", ouvrirModal);
 });
+
+async function afficherWorksDansModale() {
+    const works = await getAllWorks(); 
+    const miniaturesContainer = document.getElementById("miniatures-container"); 
+    miniaturesContainer.innerHTML = ''; 
+
+    works.forEach(work => {
+        const miniatureWrapper = document.createElement("div"); 
+        miniatureWrapper.classList.add("miniature-item"); 
+        const miniature = document.createElement("img"); 
+        miniature.src = work.imageUrl; 
+        miniature.alt = work.title; 
+        miniature.classList.add("miniature"); 
+
+        miniatureWrapper.appendChild(miniature); 
+
+        miniaturesContainer.appendChild(miniatureWrapper); 
+    })
+}
